e56bd512f0a342895405ebed7b52cf8fa425f6f2
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-04-11 23:59:58 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Write your code in this file!
+const currentUser = 'Ayse Kosova'; 
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
